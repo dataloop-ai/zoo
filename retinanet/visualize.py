e0a36908f0bf834858961ab618f0521d915b2022
aa <noamr@dataloop.ai>
2020-03-23 10:03:40 +0200

    bug fix

--- retinanet/visualize.py
+++ retinanet/visualize.py
@@ -1,14 +1,16 @@
 import numpy as np
 import time
 import argparse
+import glob
 import os
 import cv2
 from . import model
 import torch
+from shutil import copyfile
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-from zoo.dataloaders.dataloader import CocoDataset, PredDataset, collater, Resizer, AspectRatioBasedSampler, \
+from zoo.dataloaders.dataloader import CocoDataset, PredDataset , PDataset, collater, Resizer, AspectRatioBasedSampler, \
     UnNormalizer, Normalizer
 
 
@@ -20,13 +22,24 @@ def detect(home_path, checkpoint_path):
     # must have a file to predict on called "predict_on"
     pred_on_path = os.path.join(home_path, 'predict_on')
     #create output path
-    output_path = os.path.join(home_path, 'predicted_on')
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-
-    dataset_val = PredDataset(pred_on_path=pred_on_path, class_list=class_names_path,
-                             transform=transforms.Compose([Normalizer(), Resizer(min_side=608)])) #TODO make resize an input param
-    sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
+    checkpoint_name = checkpoint_path.split('.')[0]
+    output_path = os.path.join(home_path, 'predictions', checkpoint_name)
+    if not os.path.exists(os.path.join(home_path, 'predictions')):
+        os.mkdir(os.path.join(home_path, 'predictions'))
+    if os.path.exists(output_path):
+        raise Exception('there are already predictions for model: ' + checkpoint_name)
+    os.mkdir(output_path)
+    #copy annotations to predictions
+    gt_file = glob.glob(os.path.join(pred_on_path, '*groundtruth*.json'))[0]
+    if os.path.exists(gt_file):
+        if not os.path.exists(os.path.join(home_path, 'predictions', 'annotations')):
+            os.mkdir(os.path.join(home_path, 'predictions', 'annotations'))
+        copyfile(gt_file, os.path.join(home_path, 'predictions', 'annotations', gt_file.split('/')[-1]))
+    # dataset_val = PredDataset(pred_on_path=pred_on_path, class_list=class_names_path,
+    #                          transform=transforms.Compose([Normalizer(), Resizer(min_side=608)])) #TODO make resize an input param
+    dataset_val = PDataset(pred_on_path, set_name='groundtruth',
+                        transform=transforms.Compose([Normalizer(), Resizer(min_side=608)]))
+    # sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
     dataloader_val = DataLoader(dataset_val, num_workers=0, collate_fn=collater, batch_sampler=None)
     checkpoint = torch.load(checkpoint_path)
     scales = checkpoint['scales']
@@ -44,7 +57,7 @@ def detect(home_path, checkpoint_path):
         cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
 
     for idx, data in enumerate(dataloader_val):
-
+        scale = data['scale'][0]
         with torch.no_grad():
             st = time.time()
             scores, classification, transformed_anchors = retinanet(data['img'].cuda().float())
@@ -68,22 +81,29 @@ def detect(home_path, checkpoint_path):
                 label_idx = int(classification[idxs[j]])
                 label_name = dataset_val.labels[label_idx]
                 score = scores[idxs[j]].item()
-                detections_list.append([label_name, score, x1, y1, x2, y2])
-                draw_caption(img, (x1, y1, x2, y2), label_name)
 
+                draw_caption(img, (x1, y1, x2, y2), label_name)
                 cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
                 print(label_name)
-            img_name = dataset_val.image_names[idx].split('/')[-1].split('.')[0]
+                # un resize for eval against gt
+                bbox /= scale
+                bbox.round()
+                x1 = int(bbox[0])
+                y1 = int(bbox[1])
+                x2 = int(bbox[2])
+                y2 = int(bbox[3])
+                detections_list.append([label_name, str(score), str(x1), str(y1), str(x2), str(y2)])
+            img_name = dataset_val.coco.dataset['images'][idx]['file_name'].split('.')[0]
             filename = img_name + '.txt'
             filepathname = os.path.join(output_path, filename)
-            with open(filepathname, 'w') as f:
+            with open(filepathname, 'w', encoding='utf8') as f:
                 for single_det_list in detections_list:
                     for i, x in enumerate(single_det_list):
                         f.write(str(x))
                         f.write(' ')
                     f.write('\n')
 
-            img_save_name = dataset_val.image_names[idx].split('/')[-1]
+            img_save_name = dataset_val.coco.dataset['images'][idx]['file_name']
             save_to_path = os.path.join(output_path, img_save_name)
             cv2.imwrite(save_to_path, img)
             cv2.waitKey(0)
