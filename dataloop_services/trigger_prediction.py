532df871292ccb498400772cabb7bfa5e5d87d3d
aa <noamr@dataloop.ai>
2020-04-10 02:43:36 +0300

    fix up trigger

--- dataloop_services/trigger_prediction.py
+++ dataloop_services/trigger_prediction.py
@@ -1,20 +1,14 @@
-from push_deploy import push_package, deploy, x
+from .push_deploy import push_package, deploy, x
 import dtlpy as dl
 dl.setenv('prod')
+
+predict_service = dl.services.get('predict_single_item')
+
 project = dl.projects.get('golden_project')
-package = push_package(project)
-predict_service = deploy(package, service_name='predict_item')
-
-# item_input = dl.FunctionIO(type='Item', name='item', value={"item_id": ''})
-# inputs = [item_input]
-# service.execute(function_name='run', execution_input=inputs)
-
 dataset = project.datasets.get('predict_rodent')
 dataset_id = dataset.id
-filters = dl.Filters()
-filters.add(field='datasetId', values=dataset.id)
-filters.add(field='metadata.system.annotationStatus', values='completed')
 
+#TODO: where does dataset id go?
 trigger = predict_service.triggers.create(
     service_id=predict_service.id,
     resource=dl.TriggerResource.ITEM,
